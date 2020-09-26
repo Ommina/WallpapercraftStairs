88e1103e34ea3534c4fd56442c36f7c6fba47a2d
ommina <lloyd@shaw.ca>
2020-09-25 21:53:15 -0600

    Complete the 1.15.2 Update
    o Adds in in-world changing of stairs.  Requires base mod version 1.2.4 or above.

--- src/main/java/net/ommina/wallpapercraftstairs/blocks/WallpaperStairBlock.java
+++ src/main/java/net/ommina/wallpapercraftstairs/blocks/WallpaperStairBlock.java
@@ -1,8 +1,19 @@
 package net.ommina.wallpapercraftstairs.blocks;
 
-import net.minecraft.block.BlockState;
-import net.minecraft.block.StairsBlock;
+import net.minecraft.block.*;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IWorldReader;
+import net.minecraft.world.World;
 import net.ommina.wallpapercraft.blocks.IDecorativeBlock;
+import net.ommina.wallpapercraft.blocks.InWorldHelper;
+import net.ommina.wallpapercraft.items.ModItems;
+import net.ommina.wallpapercraft.items.PressColour;
+import net.ommina.wallpapercraft.items.PressVariant;
+import net.ommina.wallpapercraft.sounds.ModSoundType;
+
+import javax.annotation.Nullable;
 
 public class WallpaperStairBlock extends StairsBlock implements IDecorativeBlock {
 
@@ -19,7 +30,7 @@ public class WallpaperStairBlock extends StairsBlock implements IDecorativeBlock
 
     }
 
-//region Overrides
+    //region Overrides
     @Override
     public String getName() {
         return this.pattern + this.colour + this.suffix;
@@ -38,6 +49,50 @@ public class WallpaperStairBlock extends StairsBlock implements IDecorativeBlock
     @Override
     public String getSuffix() {
         return this.suffix;
+    }
+
+    @Override
+    public SoundType getSoundType( final BlockState state, final IWorldReader world, final BlockPos pos, @Nullable final Entity entity ) {
+
+        if ( !(entity instanceof PlayerEntity) )
+            return SoundType.STONE;
+
+        final PlayerEntity player = (PlayerEntity) entity;
+
+        if ( player.getHeldItemMainhand().isEmpty() )
+            return SoundType.STONE;
+
+        if ( player.getHeldItemMainhand().getItem() == ModItems.PAINTBRUSH || player.getHeldItemMainhand().getItem() instanceof PressColour || player.getHeldItemMainhand().getItem() instanceof PressVariant )
+            return ModSoundType.BLOCK_CHANGE;
+
+        return SoundType.STONE;
+
+    }
+
+    @Override
+    public void onBlockClicked( final BlockState state, final World world, final BlockPos pos, final PlayerEntity player ) {
+
+        Block block = Blocks.AIR;
+
+        if ( player.getHeldItemMainhand().getItem() == ModItems.PAINTBRUSH )
+            block = InWorldHelper.getIncrementedBlockColour( this );
+        else if ( player.getHeldItemMainhand().getItem() instanceof PressColour )
+            block = InWorldHelper.getBlockFromColourPress( this, (PressColour) player.getHeldItemMainhand().getItem() );
+        else if ( player.getHeldItemMainhand().getItem() instanceof PressVariant )
+            block = InWorldHelper.getBlockFromVariantPress( this, (PressVariant) player.getHeldItemMainhand().getItem() );
+
+        if ( block == Blocks.AIR )
+            return;
+
+        BlockState newState = block.getStateContainer().getBaseState()
+             .with( StairsBlock.FACING, state.get( StairsBlock.FACING ) )
+             .with( StairsBlock.HALF, state.get( StairsBlock.HALF ) )
+             .with( StairsBlock.SHAPE, state.get( StairsBlock.SHAPE ) )
+             .with( StairsBlock.WATERLOGGED, state.get( StairsBlock.WATERLOGGED ) );
+
+        if ( !world.isRemote )
+            world.setBlockState( pos, newState, 3 );
+
     }
 //endregion Overrides
 
