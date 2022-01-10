ee793c78b2c976da846003d2731e0f1d8c028d52
ommina <lloyd@shaw.ca>
2022-01-10 03:38:38 -0700

    Update to 1.18.1

--- src/main/java/net/ommina/wallpapercraftstairs/network/Network.java
+++ src/main/java/net/ommina/wallpapercraftstairs/network/Network.java
@@ -1,8 +1,8 @@
 package net.ommina.wallpapercraftstairs.network;
 
 import net.minecraft.resources.ResourceLocation;
-import net.minecraftforge.fmllegacy.network.NetworkRegistry;
-import net.minecraftforge.fmllegacy.network.simple.SimpleChannel;
+import net.minecraftforge.network.NetworkRegistry;
+import net.minecraftforge.network.simple.SimpleChannel;
 import net.ommina.wallpapercraftstairs.WallpapercraftStairs;
 
 public class Network {
